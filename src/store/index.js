f4a7994812a3ff47528c53a67607f219b8f1cf6d
TLORENT <lorent.tanguy@gmail.com>
2022-04-27 14:42:50 +0200

    [Countries.vue] first working list

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,29 @@ const store = createStore({
     current_country: {},
     countries: [],
   },
-  getters: {},
-  mutations: {},
-  actions: {}
+  getters: {
+    getCountries: state => state.countries,
+  },
+  mutations: {
+    setCountries: (state, countries_data) => state.countries = countries_data,
+  },
+  actions: {
+    loadCountries: async ({commit}) => {
+      // load the data via fetch
+      const response = await fetch('https://restcountries.com/v2/all')
+
+      if (!response.ok) throw response;
+
+      // parse the JSON response
+      const countries_data = await response.json();
+
+      // commit the new value via the "setWorldData" mutation
+      commit(
+          'setCountries',
+          countries_data,
+      );
+    }
+  }
 })
 
 export default store;
--- src/views/CountriesView.vue
+++ src/views/CountriesView.vue
@@ -1,23 +1,27 @@
 <template>
   <h1>Countries</h1>
   <div class="countries">
-    <router-link  v-for="(country, idx) in countries" :key="idx">
-      <h2 class="country__title">{{country.name}}</h2>
-    </router-link>
+    <div  v-for="(country, idx) in countries" :key="idx">
+      <h2 class="country__name">{{country.name}}</h2>
+    </div>
   </div>
 </template>
+
 <script>
 
 
 export default {
   name: 'CountriesView',
-  // computed:  {
-  //   countries() {
-
-  //     // Getters
-  //     return this.$store.getters.getCountries;
-  //   }
-  // }
+  created () {
+    // action
+    this.$store.dispatch('loadCountries');
+  },
+  computed:  {
+    countries() {
+      // Getters
+      return this.$store.getters.getCountries;
+    }
+  }
 }
 </script>
 
